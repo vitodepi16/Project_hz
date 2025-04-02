85fa1cb137ac24bf73cb928bf715d4d0dbc310bf
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2025-04-02 19:24:56 +0200

    nello lavora pls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-
-
 import './App.css'
+import HeaderComponent from './assets/Component/HeaderComponent'
+import CardComponent from './assets/Component/CardComponent'
+
+  
 
 function App() {
   
 
   return (
     <>
-  
+    
+<HeaderComponent
+  texts={['React Bits']} 
+  velocity={200} 
+  className="custom-scroll-text"
+      />
+      <CardComponent className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+Prova carda
+</CardComponent>
+      <HeaderComponent
+        texts={['React Bits']} 
+  velocity={200} 
+  className="custom-scroll-text"/>
     </>
   )
 }
---
+++ src/assets/Component/FooterComponent.tsx
@@ -0,0 +1,6 @@
+  
+<ScrollVelocity
+  texts={['React Bits', 'Scroll Down']} 
+  velocity={velocity} 
+  className="custom-scroll-text"
+/>
