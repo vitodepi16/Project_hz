31d3fa856bdf11ca8374ce521821ffbfe345ef36
NelloRossi <rossianiello97@gmail.com>
2025-04-03 19:11:50 +0200

    added container childs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import HeaderComponent from './assets/Component/HeaderComponent'
 import FooterComponent from './assets/Component/FooterComponent'
 import CardComponent from './assets/Component/CardComponent'
-
   
 
 function App() {
@@ -13,9 +12,16 @@ function App() {
     
       <HeaderComponent />
       <div className="container">
-        <CardComponent className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-          Prova card
-        </CardComponent>
+        <div className="player">VideoPlayer</div>
+        <div className="big-title">BigTitle</div>
+        <div className="socials">Socials</div>
+        <div className="vision">Vision</div>
+        <div className="cards">
+          <CardComponent className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+            Prova card
+          </CardComponent>
+        </div>
+        <div className="contacts">Contacts</div>
       </div>
       
     <FooterComponent/>
